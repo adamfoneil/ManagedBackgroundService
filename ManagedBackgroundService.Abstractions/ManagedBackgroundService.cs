18c7cf62269a04d2a87088e273908e56df264df5
adamfoneil <adamfoneil@proton.me>
2026-09-09 09:01:45 -0400

    more stuff

--- ManagedBackgroundService.Abstractions/ManagedBackgroundService.cs
+++ ManagedBackgroundService.Abstractions/ManagedBackgroundService.cs
@@ -5,54 +5,89 @@ namespace ManagedBackgroundService.Abstractions;
 
 public enum Status
 {
-    // inner loop running normally
+    /// <summary>
+    /// inner loop running normally
+    /// </summary>
     Enabled = 1,
-    // an error happened in the inner loop, so job is disabled (but outer loop is still running, so in theory it could be fixed and resume)
+    /// <summary>
+    /// an error happened in the inner loop, so job is disabled (but outer loop is still running, so in theory it could be fixed and resume)
+    /// </summary>
     Disabled = 2,
-    // an exception escapped the outer loop, meaning app must be restarted
+    /// <summary>
+    /// an exception escaped to the outer loop, app must be restarted
+    /// </summary>
     Stopped = 3
 }
 
-public abstract class ManagedBackgroundService(
-    ILogger<ManagedBackgroundService> logger) : BackgroundService
+public abstract class ManagedBackgroundService : BackgroundService
 {
-    protected readonly ILogger<ManagedBackgroundService> Logger = logger;
+    protected ILogger Logger { get; }
 
-    protected abstract Task ExecuteInternalAsync(CancellationToken stoppingToken);
+    protected ManagedBackgroundService(ILoggerFactory loggerFactory)
+    {
+        Logger = loggerFactory.CreateLogger(GetType().FullName!);
+    }
+    
+    /// <summary>
+    /// this is your custom logic for the background service
+    /// </summary>
+    protected abstract Task ExecuteInternalAsync(CancellationToken stoppingToken);    
+    /// <summary>
+    /// defined pause time between inner loop cycles
+    /// </summary>
+    protected abstract TimeSpan EnabledDelay { get; }
+    protected virtual TimeSpan DisabledDelay { get => TimeSpan.FromMinutes(1); }
 
-    protected abstract Status Status { get; }
-    protected abstract Task SetStatusAsync(Status status);
+    public Status Status { get; private set; }
+    public DateTime StatusDateTimeUtc { get; private set; }
+    public Exception? Exception { get; private set; }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         try
         {
+            Status = Status.Enabled;            
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 if (Status != Status.Enabled)
                 {
                     // if not enabled, don't execute main job, and don't spend too many cycles waiting for repair
-                    await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+                    await Task.Delay(DisabledDelay, stoppingToken);
                     continue;
                 }
 
                 try
-                {                    
+                {
                     // this is where your main logic goes
+                    Exception = null;
                     await ExecuteInternalAsync(stoppingToken);
+                    await Task.Delay(EnabledDelay, stoppingToken);
                 }
                 catch (Exception exc)
                 {
                     // catching here enables you to keep the main loop running, so you can possibly fix whatever issue happened, without restarting app
-                    await SetStatusAsync(Status.Disabled);
+                    Status = Status.Disabled;
+                    StatusDateTimeUtc = DateTime.UtcNow;
                     Logger.LogError(exc, "Error in inner loop, background service {type} disabled", GetType().Name);
+                    Exception = exc;
                 }                
             }
+
+            // if you make it here, inner loop exited
+            Status = Status.Stopped;
+            StatusDateTimeUtc = DateTime.UtcNow;
+        }
+        catch (Exception exc) when (exc is OperationCanceledException)
+        {
+            Logger.LogWarning("App is restarting, {type} job should resume", GetType().Name);
         }
         catch (Exception exc)
         {
-            // you don't want to escape inner loop. If so, app must be restarted
-            await SetStatusAsync(Status.Stopped);
+            // Catching here prevents impacting the host, but app must be restarted to resume job            
+            Status = Status.Stopped;
+            StatusDateTimeUtc = DateTime.Now;
+            Exception = exc;
             Logger.LogError(exc, "Error in outer loop, background service {type} stopped", GetType().Name);
         }
     }
