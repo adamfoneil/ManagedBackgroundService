82917a5b4b7079b893ffb568cfe9a3ae7b255fc3
adamfoneil <adamfoneil@proton.me>
2026-09-13 10:20:26 -0400

    started on web demo (Blazor Server)

---
+++ WebDemo/BackgroundJobs/SampleRecurringJob.cs
@@ -0,0 +1,15 @@
+﻿using ManagedBackgroundServices.Abstractions;
+
+namespace WebDemo.BackgroundJobs;
+
+public class SampleRecurringJob(ILoggerFactory loggerFactory) : ScheduledBackgroundService(loggerFactory, TimeProvider.System)
+{
+    protected override async Task ExecuteScheduledAsync(CancellationToken stoppingToken)
+    {
+        Logger.LogInformation("Scheduled job is running");
+        await Task.CompletedTask;
+    }
+
+    protected override DateTimeOffset GetNextRunTime(DateTimeOffset currentTime) => currentTime.Add(TimeSpan.FromSeconds(3));
+}
+ 
