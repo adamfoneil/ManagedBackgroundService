1426d892e46c9206b64b1552ec1d989fa5fedc60
adamfoneil <adamfoneil@proton.me>
2026-09-13 10:05:17 -0400

    check for paused services in health check

--- Abstractions/BackgroundServicesHealthCheck.cs
+++ Abstractions/BackgroundServicesHealthCheck.cs
@@ -9,14 +9,29 @@ public class BackgroundServicesHealthCheck(IManagedBackgroundServiceProvider bac
     public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
     {
         var unhealthy = _backgroundServices.Services
-            .Where(s => s.Status != Status.Running)
+            .Where(s => s.Status == Status.Crashed)
             .Select(s => new { s.GetType().Name, Status = s.Status.ToString() })
             .ToArray();
 
-        if (!unhealthy.Any()) return Task.FromResult(HealthCheckResult.Healthy("All managed background services running"));
+        if (unhealthy.Any())
+        {
+            return Task.FromResult(HealthCheckResult.Unhealthy(
+                $"{unhealthy.Length} managed background service(s) have crashed.",
+                data: unhealthy.ToDictionary(item => item.Name, item => (object)item.Status)));
+        }
 
-        return Task.FromResult(HealthCheckResult.Unhealthy(
-           $"{unhealthy.Length} managed background service(s) are not running.",
-           data: unhealthy.ToDictionary(item => item.Name, item => (object)item.Status)));
+        var degraded = _backgroundServices.Services
+            .Where(s => s.Status == Status.Paused)
+            .Select(s => new { s.GetType().Name, Status = s.Status.ToString() })
+            .ToArray();
+
+        if (degraded.Any())
+        {
+            return Task.FromResult(HealthCheckResult.Degraded(
+                $"{degraded.Length} managed background service(s) are paused.",
+                data: degraded.ToDictionary(item => item.Name, item => (object)item.Status)));
+        }
+
+        return Task.FromResult(HealthCheckResult.Healthy("All managed background services running"));
     }
 }
