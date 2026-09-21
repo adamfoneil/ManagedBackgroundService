7629d8478a015e5b0871b7a4c28b19b4929ff3e8
adamfoneil <adamfoneil@proton.me>
2026-09-21 13:04:29 -0400

    a more frequent test job

---
+++ WebDemo/BackgroundJobs/FrequentJob.cs
@@ -0,0 +1,14 @@
+﻿using ManagedBackgroundServices.Abstractions.Infrastructure;
+
+namespace WebDemo.BackgroundJobs;
+
+public class FrequentJob(ILogger<FrequentJob> logger) : IBackgroundWorker
+{
+    private readonly ILogger<FrequentJob> _logger = logger;
+
+    public async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("I'm doing frequent work");
+        await Task.CompletedTask;
+    }
+}
--- WebDemo/Program.cs
+++ WebDemo/Program.cs
@@ -18,7 +18,8 @@ builder.Services
 builder.Services
     .AddScheduledJob<DbCleanupJob>("*1d t[2:00am]")
     .AddScheduledJob<ReindexJob>("d[mon..fri] t[9:30am, 3:30pm]")
-    .AddScheduledJob<WeeklyReportsJob>("d[sat]");
+    .AddScheduledJob<WeeklyReportsJob>("d[sat]")
+    .AddScheduledJob<FrequentJob>("*5s");
 
 var app = builder.Build();
 
